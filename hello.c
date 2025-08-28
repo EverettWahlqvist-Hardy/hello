b960bbc145822d6798feaa0dd4c91688dae35c71
Everett Wahlqvist-Hardy <ewahlqvist-hardy2@huskers.unl.edu>
2025-08-28 11:15:46 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Everett Wahlqvist-Hardy
+ *
+ * Date: 2025/08/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Everett Wahlqvist-Hardy\n");
+
+  return 0;
+}
