9ef24e33bf9982c706749c52e7202e21f569624a
Everett Wahlqvist-Hardy <ewahlqvist-hardy2@huskers.unl.edu>
2025-09-01 18:40:11 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+* Author: Everett Wahlqvist-Hardy
+ * Date: 2025/8/26
+ *
+ * A simple program that prints name and major
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv ) {
+
+    printf("Everett Wahlqvist-Hardy\n");
+    printf("Computer Science\n");
+
+    return 0;
+}
